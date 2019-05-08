9eb6fe258f760abae85d53c8f6f74babf4702732
Jay Kickliter <jay@kickliter.com>
2019-05-08 14:31:47 -0700

    loragw: replaced atomic int with simpler atomic bool

--- loragw/src/lib.rs
+++ loragw/src/lib.rs
@@ -1,8 +1,9 @@
 use llg;
 use std::ops::Drop;
-use std::sync::atomic::{AtomicIsize, Ordering};
+use std::sync::atomic::{AtomicBool, Ordering};
 
 /// A common error type for this crate.
+#[derive(Debug)]
 pub enum Error {
     Busy,
     HAL,
@@ -23,13 +24,12 @@ pub struct Gateway {}
 //
 // TODO: This is not a great solution, since another process has its
 // own count.
-static GW_OPEN_CNT: AtomicIsize = AtomicIsize::new(0);
+static GW_IS_OPEN: AtomicBool = AtomicBool::new(false);
 
 impl Gateway {
     pub fn open() -> Result<Self, Error> {
-        // We can only have 0 or 1 instances
-        if GW_OPEN_CNT.fetch_add(1, Ordering::SeqCst) > 0 {
-            GW_OPEN_CNT.fetch_sub(1, Ordering::SeqCst);
+        // We can only 'open' one instance
+        if GW_IS_OPEN.compare_and_swap(false, true, Ordering::Acquire) {
             return Err(Error::Busy);
         }
         Ok(Gateway {})
@@ -49,7 +49,7 @@ impl Gateway {
 
 impl Drop for Gateway {
     fn drop(&mut self) {
-        assert!(GW_OPEN_CNT.fetch_sub(1, Ordering::SeqCst) >= 0);
+        GW_IS_OPEN.store(false, Ordering::Release);
     }
 }
 
@@ -66,21 +66,21 @@ mod tests {
     #[test]
     fn test_open_close_succeeds() {
         let _lock = TEST_MUTEX.lock().unwrap();
-        assert!(GW_OPEN_CNT.load(Ordering::SeqCst) == 0);
+        assert!(!GW_IS_OPEN.load(Ordering::Relaxed));
         {
             let _gw = Gateway::open().unwrap();
-            assert!(GW_OPEN_CNT.load(Ordering::SeqCst) == 1);
+            assert!(GW_IS_OPEN.load(Ordering::Relaxed));
             // _gw `drop`ped here
         }
-        assert!(GW_OPEN_CNT.load(Ordering::SeqCst) == 0);
+        assert!(!GW_IS_OPEN.load(Ordering::Relaxed));
     }
 
     #[test]
     fn test_double_open_fails() {
         let _lock = TEST_MUTEX.lock().unwrap();
-        assert!(GW_OPEN_CNT.load(Ordering::SeqCst) == 0);
+        assert!(!GW_IS_OPEN.load(Ordering::Relaxed));
         let _gw1 = Gateway::open().unwrap();
-        assert!(GW_OPEN_CNT.load(Ordering::SeqCst) == 1);
+        assert!(GW_IS_OPEN.load(Ordering::Relaxed));
         assert!(Gateway::open().is_err());
     }
 }
