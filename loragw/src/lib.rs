ef27dae0d2b9952638a9bda3fa8b00ce8f8933f1
Jay Kickliter <jay@kickliter.com>
2019-05-08 14:22:37 -0700

    loragw: add a few fns to figure out which direction we'll go

--- loragw/src/lib.rs
+++ loragw/src/lib.rs
@@ -1,25 +1,49 @@
-use lgs;
+use llg;
 use std::ops::Drop;
 use std::sync::atomic::{AtomicIsize, Ordering};
 
+/// A common error type for this crate.
+pub enum Error {
+    Busy,
+    HAL,
+}
+
+/// Converts `libloragw` return codes into a Result.
+fn into_result(code: ::std::os::raw::c_int) -> Result<(), Error> {
+    match code {
+        0 => Ok(()),
+        -1 => Err(Error::HAL),
+        _ => panic!("unexpected return code: {}", code),
+    }
+}
+
 pub struct Gateway {}
 
-// Ensures we only have 0 or 1 gateway instances opened at a
-// time.
+// Ensures we only have 0 or 1 gateway instances opened at a time.
 //
 // TODO: This is not a great solution, since another process has its
 // own count.
 static GW_OPEN_CNT: AtomicIsize = AtomicIsize::new(0);
 
 impl Gateway {
-    pub fn open() -> Result<Self, ()> {
+    pub fn open() -> Result<Self, Error> {
         // We can only have 0 or 1 instances
         if GW_OPEN_CNT.fetch_add(1, Ordering::SeqCst) > 0 {
             GW_OPEN_CNT.fetch_sub(1, Ordering::SeqCst);
-            return Err(());
+            return Err(Error::Busy);
         }
-        // TODO: initialize hardware
         Ok(Gateway {})
+    }
+
+    /// Connect to the LoRa concentrator, reset it and configure it
+    /// according to previously set parameters.
+    pub fn start(&self) -> Result<(), Error> {
+        unsafe { into_result(llg::lgw_start()) }
+    }
+
+    /// Stop the LoRa concentrator and disconnect it.
+    pub fn stop(&self) -> Result<(), Error> {
+        unsafe { into_result(llg::lgw_stop()) }
     }
 }
 
