cf85d7af08d3b99093c4b999a3d1bb0789631f6e
Jay Kickliter <jay@kickliter.com>
2019-05-23 20:00:35 +0000

    repo: add new gateway app

---
+++ gateway/build.rs
@@ -0,0 +1,15 @@
+use protobuf_codegen_pure;
+
+fn main() {
+    protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
+        out_dir: "src",
+        input: &["protos/gateway.proto"],
+        includes: &["protos"],
+        customize: protobuf_codegen_pure::Customize {
+            expose_oneof: Some(true),
+            expose_fields: Some(true),
+            ..Default::default()
+        },
+    })
+    .expect("protobuf codegen");
+}
