1a7fa14dde692900f9f22cfaae66ef604938cbcd
Jay Kickliter <jay@kickliter.com>
2019-05-24 20:39:44 +0000

    app: add proto example expressed as a test

--- gateway/src/app.rs
+++ gateway/src/app.rs
@@ -160,3 +160,17 @@ pub fn go(polling_interval: u64, print_level: u8) -> Result<(), loragw::Error> {
         thread::sleep(time::Duration::from_millis(polling_interval));
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_proto_round_trip() {
+        use crate::gateway::*;
+        use protobuf::{parse_from_bytes, Message};
+        let mut buf = Vec::new();
+        let pkt0 = RxLoraPacket::new();
+        pkt0.write_to_vec(&mut buf).unwrap();
+        let pkt1: RxLoraPacket = parse_from_bytes(&buf).unwrap();
+        assert_eq!(pkt0, pkt1);
+    }
+}
