654a4d533311206998b33896ae82507d81232d61
Jay Kickliter <jay@kickliter.com>
2019-05-28 17:56:25 +0000

    proto: remove broken test

--- messages/src/lib.rs
+++ messages/src/lib.rs
@@ -142,18 +142,3 @@ impl From<TxPacket> for loragw::TxPacketLoRa {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_proto_round_trip() {
-        use crate::gateway::*;
-        use protobuf::{parse_from_bytes, Message};
-        let mut buf = Vec::new();
-        let mut pkt0 = RxPacket::new();
-        pkt0.dummy = 42;
-        pkt0.write_to_vec(&mut buf).unwrap();
-        let pkt1: RxPacket = parse_from_bytes(&buf).unwrap();
-        assert_eq!(pkt0.dummy, pkt1.dummy);
-    }
-}
