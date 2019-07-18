24ec2be61e29cb297c5b6e24cd45945479a17abd
Louis Thiery <louis@helium.com>
2019-07-18 15:42:27 -0700

    protos is proto - a submodule

--- messages/build.rs
+++ messages/build.rs
@@ -5,8 +5,8 @@ fn main() {
 
     protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
         out_dir: "src",
-        input: &["protos/gateway.proto"],
-        includes: &["protos"],
+        input: &["proto/src/longfi.proto", "proto/src/radio.proto",],
+        includes: &["proto"],
         customize: protobuf_codegen_pure::Customize {
             expose_oneof: Some(true),
             expose_fields: Some(true),
