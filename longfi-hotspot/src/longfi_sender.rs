1b00afd8d29bebb6ab5554d674edf615944ba29b
Louis Thiery <louis@helium.com>
2019-10-23 07:13:28 -0700

    simplify sender and enforce ACK

--- longfi-hotspot/src/longfi_sender.rs
+++ longfi-hotspot/src/longfi_sender.rs
@@ -21,17 +21,11 @@ impl LongFiSender {
     pub fn tx_resp(&mut self) -> Option<LongFiResponse> {
         debug!("[LongFi] Radio has signalled that packet was sent");
 
-        let mut clear_pending_fragments = false;
-
-        let ret = match &mut self.pending_fragments {
+        match &mut self.pending_fragments {
             // if there is a vector, we should have more fragments
             Some(vec) => {
                 debug!("[LongFi] Sending another fragment. {} remaining", vec.len());
                 let maybe_fragment = vec.pop_front();
-
-                if vec.is_empty() {
-                    clear_pending_fragments = true;
-                }
 
                 match maybe_fragment {
                     Some(fragment) => {
@@ -43,30 +37,34 @@ impl LongFiSender {
 
                         Some(LongFiResponse::RadioReq(fragment))
                     }
-                    None => None,
-                }
-            }
-            // if None, just completed a full packet
-            None => match self.req_id.take() {
-                Some(id) => {
-                    debug!("[LongFi] All packet fragments transmitted");
-                    Some(LongFiResponse::ClientResp(msg::LongFiResp {
-                        id,
-                        kind: Some(msg::LongFiResp_oneof_kind::tx_status(msg::LongFiTxStatus {
-                            success: true,
+                    None => {
+                        let id = match self.req_id.take() {
+                            Some(id) => id,
+                            None => 0,
+                        };
+                        debug!("[LongFi] All packet fragments transmitted");
+                        Some(LongFiResponse::ClientResp(msg::LongFiResp {
+                            id,
+                            kind: Some(msg::LongFiResp_oneof_kind::tx_status(msg::LongFiTxStatus {
+                                success: true,
+                                ..Default::default()
+                            })),
                             ..Default::default()
-                        })),
-                        ..Default::default()
-                    }))
+                        })))
+                    }
                 }
-                None => None,
+            // if None, just completed a full packet
+            None => {
+                Some(LongFiResponse::ClientResp(msg::LongFiResp {
+                    id,
+                    kind: Some(msg::LongFiResp_oneof_kind::tx_status(msg::LongFiTxStatus {
+                        success: true,
+                        ..Default::default()
+                    })),
+                    ..Default::default()
+                })))
             },
         };
-
-        if clear_pending_fragments {
-            self.pending_fragments = None;
-        }
-        ret
     }
 
     pub fn transmit(&mut self, tx_uplink: &msg::LongFiTxPacket) -> Option<LongFiResponse> {
