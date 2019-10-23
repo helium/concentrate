199bbccc6719585c29e7b7b006e1a9eced1c11b8
Louis Thiery <louis@helium.com>
2019-10-23 07:20:45 -0700

    simplify tx_resp logic

--- longfi-hotspot/src/longfi_sender.rs
+++ longfi-hotspot/src/longfi_sender.rs
@@ -6,7 +6,7 @@ use std::collections::VecDeque;
 pub struct LongFiSender {
     rng: rand::ThreadRng,
     req_id: Option<u32>,
-    pending_fragments: Option<VecDeque<msg::RadioReq>>,
+    pending_fragments: VecDeque<msg::RadioReq>,
 }
 
 impl LongFiSender {
@@ -14,47 +14,33 @@ impl LongFiSender {
         LongFiSender {
             rng: rand::thread_rng(),
             req_id: None,
-            pending_fragments: None,
+            pending_fragments: VecDeque::new(),
         }
     }
 
     pub fn tx_resp(&mut self) -> Option<LongFiResponse> {
         debug!("[LongFi] Radio has signalled that packet was sent");
 
-        match &mut self.pending_fragments {
-            // if there is a vector, we should have more fragments
-            Some(vec) => {
+        let vec = &mut self.pending_fragments;
+
+        match vec.pop_front() {
+            Some(fragment) => {
                 debug!("[LongFi] Sending another fragment. {} remaining", vec.len());
-                let maybe_fragment = vec.pop_front();
-
-                match maybe_fragment {
-                    Some(fragment) => {
-                        // print the fragment payload nicely
-                        if let Some(msg) = &fragment.kind {
-                            let msg::RadioReq_oneof_kind::tx(tx) = msg;
-                            debug!("[LongFi] Fragment: {:?}", tx.payload);
-                        }
-
-                        Some(LongFiResponse::RadioReq(fragment))
-                    }
-                    None => {
-                        let id = match self.req_id.take() {
-                            Some(id) => id,
-                            None => 0,
-                        };
-                        debug!("[LongFi] All packet fragments transmitted");
-                        Some(LongFiResponse::ClientResp(msg::LongFiResp {
-                            id,
-                            kind: Some(msg::LongFiResp_oneof_kind::tx_status(msg::LongFiTxStatus {
-                                success: true,
-                                ..Default::default()
-                            })),
-                            ..Default::default()
-                        })))
-                    }
+                // print the fragment payload nicely
+                if let Some(msg) = &fragment.kind {
+                    let msg::RadioReq_oneof_kind::tx(tx) = msg;
+                    debug!("[LongFi] Fragment: {:?}", tx.payload);
                 }
-            // if None, just completed a full packet
+
+                Some(LongFiResponse::RadioReq(fragment))
+            }
+            // no fragments means we are all done
             None => {
+                let id = match self.req_id.take() {
+                    Some(id) => id,
+                    None => 0,
+                };
+                debug!("[LongFi] All packet fragments transmitted");
                 Some(LongFiResponse::ClientResp(msg::LongFiResp {
                     id,
                     kind: Some(msg::LongFiResp_oneof_kind::tx_status(msg::LongFiTxStatus {
@@ -62,9 +48,9 @@ impl LongFiSender {
                         ..Default::default()
                     })),
                     ..Default::default()
-                })))
-            },
-        };
+                }))
+            }
+        }
     }
 
     pub fn transmit(&mut self, tx_uplink: &msg::LongFiTxPacket) -> Option<LongFiResponse> {
